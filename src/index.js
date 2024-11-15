c2ddc633f0baa5f82f1447d2d5f36dead40e7634
zohairasif <zoasif@ethz.ch>
2024-11-15 21:30:21 +0100

    hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Changed to HashRouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
