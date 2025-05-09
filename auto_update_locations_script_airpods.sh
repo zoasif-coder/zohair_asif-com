4df717d2d257949fe7d118399d4767f84d8281f0
zohairasif <zoasif@ethz.ch>
2025-05-09 10:44:48 +0200

    added new location 2025-05-09 10:44:48

---
+++ auto_update_locations_script_airpods.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Open Find My app and wait
+open -a "Find My"
+sleep 5
+
+cd /Users/zohairasif/zohair_asif-com/Find-My-Mapper/mapper
+# Activate Python virtual environment
+source .findmy/bin/activate
+
+# Run the tracker script
+.findmy/bin/python device_tracker.py
+
+# Copy CSV to the website folder
+cp /Users/zohairasif/zohair_asif-com/Find-My-Mapper/mapper/locations.csv /Users/zohairasif/zohair_asif-com/src/assets/coords/locations.csv
+
+# Deploy site
+cd /Users/zohairasif/zohair_asif-com
+npm run deploy
+
+# Commit and push
+git add .
+git commit -m "added new location $(date '+%Y-%m-%d %H:%M:%S')"
+git push
